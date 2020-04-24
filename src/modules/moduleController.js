2800caf612be9b5e7def18b7d6370cd28a3cc506
skywarth <yigitk.ersoy@gmail.com>
2020-04-25 00:50:54 +0300

    fake image creator ready, movin' on to read process for 1D array

--- src/modules/moduleController.js
+++ src/modules/moduleController.js
@@ -3,7 +3,8 @@ export class faker{
         this.x = x;
         this.y=y;
         this.color=colorStatus;
-        if(this.color==true){
+        this.fakeImage=this.generateFakeImage();
+        if(this.color){
             this.multiplier=3;
         }
         else{
@@ -11,9 +12,28 @@ export class faker{
         }
     }
 
+    generateFakeImage(){
+        let fakeImage=[];
+        for(let i=0;i<=this.x;i++){
+            fakeImage[i]=[];
+            for(let k=0;k<=this.y;k++){
+            fakeImage[i][k]= Math.floor((Math.random() * 100) + 1);
+            }
+        }
+        return fakeImage;
+    }
+
+    convertTo1DArray(){
+
+    }
+
 
     calcLinearArraySize(){
         return this.x*this.y*this.multiplier;
+    }
+
+    createArray(){
+       // return new array
     }
 
 }
--- test.js
+++ test.js
@@ -3,4 +3,6 @@ import {faker} from "./src/modules/moduleController.js";
 
 let testFaker=new faker(1024,768,false);
 
-alert();
+
+
+alert(testFaker.fakeImage[0][2]);
