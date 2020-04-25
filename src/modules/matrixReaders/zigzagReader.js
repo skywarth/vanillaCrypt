a737daac49d2a86e087fea6a4248fe088142277f
skywarth <yigitk.ersoy@gmail.com>
2020-04-25 04:03:06 +0300

    major bug in zigzag after half the diagonal

--- src/modules/matrixReaders/zigzagReader.js
+++ src/modules/matrixReaders/zigzagReader.js
@@ -1,4 +1,4 @@
-class tracker{
+export class tracker2{
     constructor() {
         this.x=0;
         this.y=0;
@@ -6,38 +6,61 @@ class tracker{
 }
 
 
-function zigzagReader(arr) {
-    let dimensions = [ arr.length, arr[0].length ];
+export function zigzagReader(arr) {
+    //let dimensions = [ arr.length, arr[0].length ];
 
-    let rows=arr.length-1;
-    let cols=arr[0].length-1;
+    let rows=arr.length;
+    let cols=arr[0].length;
 
     let smallestDimension=Math.min(rows,cols);
     let maxDiag=rows+cols-1;
 
     let capture=[];
-    for(let i=0;i<=rows*cols;i++){
+    /*for(let i=0;i<=rows*cols;i++){
 
-    }
+    }*/
 
 
     let counter=0;
-    capture.push(arr[0,0]);//first diagonal cleared
-    let tracker=new tracker();
+    capture.push(arr[0][0]);//first diagonal cleared
+    let tracker=new tracker2();
     let diagSelector=1;
 
     let finishStat=true;//true For UP
+
+
     while(diagSelector<=maxDiag){
     diagSelector++;
-    let diagCounter=Math.max(tracker.x,tracker.y);
+    //let diagCounter=Math.max(tracker.x,tracker.y);
     //dash to the diag
     /*if(tracker.x=0,tracker.y=0){
         tracker.y++;
     }*/
-    if(finishStat){
-        tracker.y++;
-    }
 
+
+    /*Dash to next diag*/
+    if(finishStat){
+        if(tracker.x===0 && tracker.y===cols-1){
+            //right-top corner
+            tracker.x++;
+        }
+        else{
+            tracker.y++;
+        }
+
+
+    }
+    else{
+        if(tracker.y===0 && tracker.x===rows-1){
+            //left-bottom corner
+            tracker.y++;
+        }
+        else{
+            tracker.x++;
+        }
+
+    }
+        /*Dash to next diag*/
 
 
 
@@ -49,20 +72,46 @@ function zigzagReader(arr) {
     }
     else
     {
-     readAmount=smallestDimension-diagSelector;
+     readAmount=2*smallestDimension-diagSelector;
     }
     /* Calc read amount */
-
-
-    for(let m=0;m<=readAmount;m++){
-    //reading
-        if(finishStat){
+    if(finishStat){
             //reading to down
-
+        for(let m=1;m<=readAmount;m++){
+            //reading
+            capture.push(arr[tracker.x][tracker.y]);
+            tracker.x++;
+            tracker.y--;
         }
+
+            finishStat=false;
+
+
+        tracker.x--;//recalibration
+        tracker.y++;//recalibration
+        /*if(tracker.x===rows-1){
+            finishStat=true;
+        }*/
+    }
+    else{
+        //reading to up
+        for(let m=1;m<=readAmount;m++){
+            //reading
+            capture.push(arr[tracker.x][tracker.y]);
+            tracker.x--;
+            tracker.y++;
+        }
+        finishStat=true;
+        tracker.x++;//recalibration
+        tracker.y--;//recalibration
+        /*if(tracker.y===cols-1){
+            finishStat=false;
+        }*/
     }
 
-    }
 
+
+    }
+return capture;
 
 }
--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 
 import {faker} from "./src/modules/moduleController.js";
+import {zigzagReader,tracker2} from "./src/modules/matrixReaders/zigzagReader.js";
 
 let testFaker=new faker(1024,768,false);
 
-
-
-alert(testFaker.fakeImage[0][2]);
+//let test11=[[1,2,3],[4,5,6],[7,8,9]];//success
+let test11=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
+console.log(test11);
+let ret=zigzagReader(test11);
+console.log(ret);
